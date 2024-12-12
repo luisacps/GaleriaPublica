783cdb7f80825a44e432087509d9f68a2322c87d
Luísa C. Pimentel dos Santos <luisacpsantos@gmail.com>
2024-12-12 12:33:39 -0300

    chatisse

--- app/src/main/java/santos/pimentel/galeriapublica/MainActivity.java
+++ app/src/main/java/santos/pimentel/galeriapublica/MainActivity.java
@@ -1,14 +1,28 @@
 package santos.pimentel.galeriapublica;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottonNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+
+        bottonNavigationView = findViewById(R.id.btNav);
+        bottonNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                vm.setNavigationOpSelected(item.getItemId());
+                switch (item.getItemId()) {
+                    case R.id.gridViewOp:
+                        GridViewFragment gridViewFragment = GridViewFragment.newInstance();
+                        setFragment(gridViewFragment);
+                        break;
+                    case R.id.listViewOp:
+                        ListViewFragment listViewFragment = ListViewFragment.newInstance();
+                        setFragment(listViewFragment);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragContainer, fragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    //metodo que cria um inflador de menu, adicionando o menu a activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.botton_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
     }
 }
